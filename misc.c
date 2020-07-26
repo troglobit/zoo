3c23e481f80a687eabd3f2846943d6df629170f8
Joachim Nilsson <troglobit@gmail.com>
2020-07-27 01:02:27 +0200

    Import Debian 04-fix-fullpath-buffer-overflow.patch, CVE-2006-0855
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- misc.c
+++ misc.c
@@ -136,11 +136,14 @@ if available, else the short filename is used.
 char *fullpath (direntry)
 struct direntry *direntry;
 {
-	static char result[PATHSIZE];
+	static char result[PATHSIZE+PATHSIZE+12]; // Room for enough space
 	combine (result,
 				direntry->dirlen != 0 ? direntry->dirname : "", 
 				(direntry->namlen != 0) ? direntry->lfname : direntry->fname
 			  );
+	if (strlen (result) >= PATHSIZE) {
+		prterror ('f', "Combined dirname and filename too long\n");
+	}
 	return (result);
 }
 
