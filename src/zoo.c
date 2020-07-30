aa9c6eeb7aa0d36c946220132aa3d80636d8e0ff
Joachim Nilsson <troglobit@gmail.com>
2020-07-30 09:49:14 +0200

    zoo: Modernize usage and help texts a bit and add -help command
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- src/zoo.c
+++ src/zoo.c
@@ -53,14 +53,15 @@ int main(argc,argv)
 #ifdef OOZ
 #else
 /* else not OOZ */
-      static char usage[] = "Usage: zoo {acDeglLPTuUvx}[aAcCdEfInmMNoOpPqu1:/.@n] archive file\n(\"zoo h\" for help, \"zoo H\" for extended help)\n";
+      static char usage[] =
+	 "zoo {acDeglLPTuUvx}[aAcCdEfInmMNoOpPqu1:/.@n] archive [file(s)]\n";
       static char nov_usage[] =
-          "\nNovice usage:  zoo -cmd archive[.zoo] file...  where -cmd is one of these:\n";
+	 "zoo -cmd archive[.zoo] [file(s)]  where -cmd is one of:\n";
       char *option;
 
       static char nov_cmds[] =
          /* ADD=0EXT=5    MOV=14TES=20PRI=26 DEL=33  LIS=41UPD=47  FRE=55   COMMENT=64 */
-           "-add -extract -move -test -print -delete -list -update -freshen -comment\n";
+           "-add -extract -move -test -print -delete -list -update -freshen -comment -help\n";
 
 #ifdef NOENUM
 #define NONE   -1
@@ -74,13 +75,14 @@ int main(argc,argv)
 #define UPDATE 47
 #define FRESHEN   55
 #define COMMENT   64
+#define HELP      73
 
 int cmd = NONE;
 
 #else
    enum choice {
       NONE = -1, ADD = 0, EXTRACT = 5, MOVE = 14, TEST = 20, PRINT = 26,
-      DELETE = 33, LIST = 41, UPDATE = 47, FRESHEN = 55, COMMENT = 64
+      DELETE = 33, LIST = 41, UPDATE = 47, FRESHEN = 55, COMMENT = 64, HELP = 73
    };
    enum choice cmd = NONE;          /* assume no Novice command */
 #endif
@@ -130,24 +132,24 @@ int cmd = NONE;
    option = str_dup(argv[1]);
 
 #ifdef TRACE_IO
-   if (*option == ':') {         /* for debugging output */
-      verbose++;
-      option++;                  /* hide the : from other functions */
+   if (*option == ':') {	/* for debugging output */
+       verbose++;
+       option++;		/* hide the : from other functions */
    }
 #endif
 
 #ifdef WAIT_PROMPT
    if (*option == 'w') {
-		option++;						/* hide w from other functions */
-		wait_return();
-	}
+       option++;		/* hide w from other functions */
+       wait_return();
+   }
 #endif /* WAIT_PROMPT */
 
    if (*option == 'H') ms_help(option);
    if (*option == 'h' || *option == 'H')
-      goto bigusage;
-	if (strchr("-acDegflLPTuUvVx", *option) == NULL)
-		goto give_list;
+       goto bigusage;
+   if (strchr("-acDegflLPTuUvVx", *option) == NULL)
+       goto give_list;
 
    if (*option == '-') {
 
@@ -159,6 +161,8 @@ int cmd = NONE;
 
       if (strlen(option) < 2 || cmd == NONE)
          goto show_usage;
+      if (cmd == HELP)
+	 goto bigusage;
       if (  ((cmd == ADD || cmd == MOVE || cmd == FRESHEN ||
                   cmd == UPDATE || cmd == DELETE) && argc < 4) ||
             ((cmd == EXTRACT || cmd == TEST || cmd == LIST ||
@@ -215,8 +219,8 @@ encode() will use all of it. */
    in_buf_adr = out_buf_adr + OUT_BUF_SIZE + (FUDGE/2);
 
 #ifdef OOZ
-zooext(zooname, "\0");     /* just extract -- no fancy stuff   */
-zooexit (0);                  /* and exit normally                */
+   zooext(zooname, "\0");	/* just extract -- no fancy stuff   */
+   zooexit (0);			/* and exit normally                */
 #else
 /* else not OOZ -- parse command line and invoke a routine */
    if (cmd != NONE) {
@@ -244,17 +248,17 @@ zooexit (0);                  /* and exit normally                */
          case 'T':   
             zooadd (zooname, filecount, &argv[3], option); break;
 #ifdef FILTER
-			case 'f':
-				zoofilt (option);  break;
+         case 'f':
+	    zoofilt (option);  break;
 #endif /* FILTER */
          case 'D':
             zoodel (zooname, option, 1); break;
          case 'U':
             zoodel (zooname, option, 0); break;
-			case 'g':
-				zoodel (zooname, option, 2); break;
+	 case 'g':
+	    zoodel (zooname, option, 2); break;
          case 'v':
-			case 'V':
+	 case 'V':
          case 'l': 
             zoolist(&argv[2], option, 1); break;
          case 'L': 
@@ -273,58 +277,72 @@ zooexit (0);                  /* and exit normally                */
 
 /* usage list including Novice commands */
 show_usage:
-   fprintf (stderr, "%s v%s\n\n%s%s%s", PACKAGE, VERSION, usage, nov_usage, nov_cmds);
+   printf("zoo: missing command, please specify one of 'acDeglLPTuUvx' or '-cmd' syntax\n"
+	  "Try 'zoo -h', 'zoo H', or the manual page zoo(1) for more information.\n");
    zooexit (0);
 
 /* brief usage list */
 give_list:
-   fprintf (stderr, "%s", usage); zooexit (1);
+   printf ("Usage: %s", usage);
+   zooexit (1);
 
 /* help screen */
 bigusage:
-   printf ("Zoo archiver v%s\n", PACKAGE_VERSION);
-
-   printf ("%s", usage);
-   printf ("\nChoose a command from within {} and zero or more modifiers from within [].\n");
-
-   printf ("E.g.:  `zoo a save /bin/*' will archive all files in /bin into save.zoo.\n");
-   printf ("(Please see the user manual for a complete description of commands.)\n");
-   printf ("%s", nov_usage);
-   printf ("%s", nov_cmds);
+   printf ("Zoo archiver %s\n", PACKAGE_VERSION);
    printf ("\n");
+//   printf ("%s", usage);
+
+//   printf ("%s", nov_usage);
+//   printf ("%s", nov_cmds);
+//   printf ("\n");
    wait_return();	/* print msg & wait for RETURN */
 
-   printf ("\n");
-   printf ("%s", usage);
+   printf ("Usage:\n"
+	   "  zoo COMMAND[OPTION] archive [file(s)]\n"
+	   "  zoo -cmd archive [file(s)]\n"
+	   "\n");
 
-   printf (" Commands in {} mean:         |Modifiers in [] mean:\n");
-
-   printf ("  a     add files             | a     show archive name(s) in listing\n");
-   printf ("  c     update comments       | A     apply g or c to archive\n");
-   printf ("  D     delete stored files   | c     add/list comments\n");
-   printf ("  e,x   extract files         | d     extract/list deleted files too\n");
-   printf ("  g     adj. gen. limit/count | dd    extract/list only deleted files\n");
-   printf ("  l,L,v,V list filenames      | E     erase backup after packing\n");
-   printf ("  P     pack archive          | f     fast add (no compression) or list\n");
-   printf ("  T     fix archive datestamp | M     move when adding (erase original)\n");
-   printf ("  u     add only newer files  | n     add only files not already in archive\n");
-   printf ("  U     undelete stored files | N     send extracted data to Nowhere\n");
+   printf ("Commands:                      | Options:\n");
+   printf ("  a     add files              |   a     show archive name(s) in listing\n");
+   printf ("  c     update comments        |   A     apply g or c to archive\n");
+   printf ("  D     delete stored files    |   c     add/list comments\n");
+   printf ("  e,x   extract files          |   d     extract/list deleted files too\n");
+   printf ("  g     adj. gen. limit/count  |   dd    extract/list only deleted files\n");
+   printf ("  l,L,v,V list filenames       |   E     erase backup after packing\n");
+   printf ("  P     pack archive           |   f     fast add (no compression) or list\n");
+   printf ("  T     fix archive datestamp  |   M     move when adding (erase original)\n");
+   printf ("  u     add only newer files   |   n     add only files not already in archive\n");
+   printf ("  U     undelete stored files  |   N     send extracted data to Nowhere\n");
 #ifdef FILTER
-   printf ("  f     act as filter         | c/u   compress/uncompress as filter\n");
+   printf ("  f     act as filter          |   c/u   compress/uncompress as filter\n");
 #endif /* FILTER */
-   printf (" -----------------------------  O     don't ask \"Overwrite?\"\n");
-   printf ("  q     be quiet                p     pipe extracted data to standard output\n");
-   printf ("  :     don't store dir names   /,//  extract full pathnames\n");
-   printf ("  .     pack to current dir     I     add filenames read from stdin\n");
-   printf ("  C     show file CRC value     +/-   enable/disable generations\n");
-   printf ("  S     overwrite newer files   g     list generation limits\n");
-   printf ("  P     pack after adding       @n    start extract/list at position n\n");
+   printf (" ------------------------------'   O     don't ask \"Overwrite?\"\n");
+   printf ("  q     be quiet                   p     pipe extracted data to standard output\n");
+   printf ("  :     strip any pathnames        /,//  extract/create full pathnames (default)\n");
+   printf ("  .     pack to current dir        I     add filenames read from stdin\n");
+   printf ("  C     show file CRC value        +/-   enable/disable generations\n");
+   printf ("  S     overwrite newer files      g     list generation limits\n");
+   printf ("  P     pack after adding          @n    start extract/list at position n\n");
 
 #ifdef FATTR
-   printf ("  m     list file modes         OO    overwrite read-only files\n");
+   printf ("  m     list file modes            OO    overwrite read-only files\n");
 #endif /* FATTR */
-   printf ("  C     change archive cmnt     h     use high-compression method\n");
+   printf ("  C     change archive cmnt        h     use high-compression method\n");
 #endif /* end of not OOZ */
+   printf ("\nNovice commands:\n");
+   printf (" -add      add files              -extract  extract files\n");
+   printf (" -freshen  add if older exists    -delete   delete matching files from archive\n");
+   printf (" -update   add or freshen file    -move     move into archive from filesystem\n");
+   printf (" -print    print files to stdout  -list     Show info about files in archive\n");
+   printf (" -test     check archive          -comment  Add or modify archive comment\n");
+   printf ("\n");
+   printf ("Examples:\n");
+   printf ("  zoo a  save /bin/*   # Create save.zoo from all files in /bin\n");
+   printf ("  zoo x  save          # Extract save.zoo with full paths in current directory\n");
+   printf ("  zoo x: save          # Extract files in save.zoo to current directory\n");
+   printf ("  zoo l  save          # List contents of save.zoo\n");
+   printf ("-------------------------------------------------------------------------------\n");
+   printf ("See also 'zoo H', or the manual page zoo(1) for more information.\n");
 
    /* NOTE: if allowed to fall through and return without an exit() statement,
     *       it was printing garbage--corrupted stack?  Why--bug in Microsoft C?
@@ -337,9 +355,8 @@ bigusage:
 
 /* help screens */
 static char *scrn1[] = {
-   "",
-	"command line format:",
-	"  zoo {command}[options] archive files(s)",
+	"Usage:",
+	"  zoo COMMAND[OPTIONS] archive files(s)",
 	"",
 	"Commands:",
 	" a  add files",
@@ -360,11 +377,11 @@ static char *scrn1[] = {
 };
 
 static char *scrn2[] = {
-   "",
+	"",
 	" e  extract files",
 	" x  extract files",
 	"    : - extract to current directory (ignore pathnames)",
-   "    . - make absolute pathnames relative to current directory",
+	"    . - make absolute pathnames relative to current directory",
 	"        (name /etc/hosts becomes ./etc/hosts)",
 	"    / - extract to subdirs, // create subdirs as needed - default",
 	"        (the sequence :/ may be used to use but not create subdirs)",
@@ -383,7 +400,7 @@ static char *scrn2[] = {
 };
 
 static char *scrn3[] = {
-   "",
+	"",
 	" l  list archive info",
 	" L  list info for multiple archives",
 	" v  list verbose file info and archive comment",
@@ -407,7 +424,7 @@ static char *scrn3[] = {
 };
 
 static char *scrn4[] = {
-   "",
+	"",
 	" P  pack archive, remove deleted or overwritten files",
 	"    E - erase the .bak file when done",
 	"",
@@ -431,25 +448,14 @@ static char *scrn4[] = {
 static char *scrn5[] = {
    "",
 	"Examples:",
-	"",
-	"# just add a few files",
-	"    zoo a arch file1 files",
-	"# add C source files in subdirectories",
-	"    zoo a test part1/*.c part2/*.c",
-	"# add documentation files with high compression",
-	"    zoo ah test *.doc",
-	"",
-	"# extract all files",
-	"    zoo x test",
-	"# extract files into the current directory",
-	"    zoo x: test",
-	"# extract a single file and sort before listing",
-	"    zoo xp test users.lst | sort",
-	"",
-	"# list the contents and archive comments",
-	"    zoo v arch",
-	"# list all files in all archives",
-	"    zoo L xxx.zoo /doc/*.zoo ../*.zoo",
+	"  zoo a arch file1 files             # just add a few files",
+	"  zoo a test part1/*.c part2/*.c     # add C source files in subdirectories",
+	"  zoo ah test *.doc                  # add all .doc files with high compression",
+	"  zoo x test                         # extract all files",
+	"  zoo x: test                        # extract files to current directory",
+	"  zoo xp test users.lst | sort       # extract a single file and sort before listing",
+	"  zoo v arch                         # list the contents and archive comments",
+	"  zoo L xxx.zoo /doc/*.zoo ../*.zoo  # list all files in all archives",
 	(char *)NULL
 };
 
