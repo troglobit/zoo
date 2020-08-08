3b9c5a4818572808c5f9086ead78f6a1dc9d67cf
Joachim Nilsson <troglobit@gmail.com>
2020-08-08 10:53:01 +0200

    Use defult comment indent for zoo, don't garble comments
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- indent.sh
+++ indent.sh
@@ -5,7 +5,7 @@
 # that we've added, so indent doesn't insert spaces in odd places. 
 # 
 
-indent --linux-style -bad -fca -c40 -cd40 -cp40 -d8 --line-length120 \
+indent --linux-style -bad -c42 -cd42 -cp42 -d8 --line-length120 \
        -T size_t -T sigset_t -T timeval_t -T pid_t -T pthread_t \
        -T time_t -T uint32_t -T uint16_t -T uint8_t -T socklen_t \
        -T node   -T uchar    -T uint     -T ushort  -T ulong \
