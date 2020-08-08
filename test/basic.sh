5b854c99792d0a8d4a2541a84495ff9ccd660514
Joachim Nilsson <troglobit@gmail.com>
2020-08-08 11:21:39 +0200

    Provoke integrity check error if regression in 32/64 bit
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- test/basic.sh
+++ test/basic.sh
@@ -11,10 +11,15 @@ dst=`mktemp -d -p .`
 
 zoo a $ark ${srcdir}/../doc/*
 
+# Also verify adding more files work as intended.  32/64-bit problems
+# with long in struct zoo_header caused "integrity check" issues.
+zoo a $ark ${srcdir}/../src/*
+
 cd ${dst}
 zoo x $ark
 cd -
 
+# Only verify doc, 2nd src/* step verifies integrity only
 diff -r ${srcdir}/../doc ${dst}/doc
 
 rm -rf ${dst}
